cf336e4c09a9f97d4c36ea6e6f449e91e425616a
Norm <tehnorm@users.noreply.github.com>
2023-04-13 00:50:22 +0000

    Apply fixes from StyleCI

--- src/Util/Timer.php
+++ src/Util/Timer.php
@@ -2,27 +2,27 @@
 
 namespace Supabase\Util;
 
-class Timer 
+class Timer
 {
-    private $timer;
-    private $tries = 0;
+	private $timer;
+	private $tries = 0;
 
-    public function __construct($timerFn, $fn)
-    {
-        $this->timer = $timerFn;
-        $this->fn = $fn;
-    }
+	public function __construct($timerFn, $fn)
+	{
+		$this->timer = $timerFn;
+		$this->fn = $fn;
+	}
 
-    public function reset()
-    {
-        $this->tries = 0;
-    }
+	public function reset()
+	{
+		$this->tries = 0;
+	}
 
-    public function schedule()
-    {
-        $timelimit = $this->timer();
-        $this->tries++;
-        sleep($timelimit);
-        $this->fn();
-    }
+	public function schedule()
+	{
+		$timelimit = $this->timer();
+		$this->tries++;
+		sleep($timelimit);
+		$this->fn();
+	}
 }
